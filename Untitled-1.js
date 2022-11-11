da1164ce1fb13ff1b73c54fd5d5f46cd83ed5d6f
yograj44 <118001586+yograj44@users.noreply.github.com>
2022-11-11 20:34:47 +0530

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,33 @@
+
+function formvalidation() {
+ 
+  const username = [
+    "Saab",
+    "Volvo",
+    "BMW"
+  ];
+
+
+  // let usernameVar = document.forms["registerationform"]["Username"].value;
+
+  // if(username.getElementById(usernameVar)==true){
+  //     alert("use r alredy eixt")
+  // }
+
+  // let emailbox = document.forms[""]
+
+  // if (container == "") {
+  //   document.getElementById("errordiv").innerHTML = "Username cannot be empty";
+
+  // }
+
+  for(let i =1;i<=88;i++){
+    console.log(i);
+  }
+
+
+
+
+
+
+}
